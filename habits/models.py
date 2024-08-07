e01d8a23adb8ccbf01ca7498ac940fa292b7c767
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-08-07 19:00:43 +0300

    Create habit endpoint

---
+++ habits/models.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.db import models
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class Habit(models.Model):
+    FREQUENCY_UNITS = [
+        ("minutes", "минуты"),
+        ("hours", "часы"),
+        ("days", "дни"),
+    ]
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Cоздатель привычки",
+        **NULLABLE,
+    )
+    place = models.CharField(max_length=200, verbose_name="Место привычки")
+    time = models.DateTimeField(verbose_name="Дата и время привычки", auto_now_add=True)
+    action = models.CharField(max_length=200, verbose_name="Действие")
+    is_pleasant = models.BooleanField(verbose_name="Приятная")
+    related_habit = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, **NULLABLE, verbose_name="Связанная привычка"
+    )
+    frequency_number = models.PositiveIntegerField(verbose_name="Количество раз")
+    frequency_unit = models.CharField(
+        max_length=10,
+        choices=FREQUENCY_UNITS,
+        default="days",
+        verbose_name="Единицы измерения",
+    )
+    reward = models.CharField(max_length=200, verbose_name="Вознаграждение", **NULLABLE)
+    duration = models.DurationField(verbose_name="Длительность действия")
+    is_public = models.BooleanField(default=True, verbose_name="Публичная")
+
+    def __str__(self):
+        return f"{self.user} - {self.action}"
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
---
+++ users/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="email")
+    avatar = models.ImageField(
+        upload_to="users/avatars", verbose_name="Аватар", **NULLABLE
+    )
+
+    tg_chat_id = models.CharField(
+        max_length=50, verbose_name="Телеграм chat-id", **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
